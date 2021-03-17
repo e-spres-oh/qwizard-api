02f011d437c2c7e479430d1376b8eb1c47aa1333
Sorin Guga <sorin.guga@unifiedpost.com>
2021-03-17 22:25:33 +0200

    Enables STDOUT sync

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 require "active_support/core_ext/integer/time"
 
+STDOUT.sync = true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
