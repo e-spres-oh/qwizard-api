d39cd4eac686641cf08ccdd9e046d7e7bbec79ff
Sorin Guga <sorin.guga@unifiedpost.com>
2021-03-17 15:50:45 +0200

    week-1-homework - Fixes Rubocop errors

--- example.rb
+++ example.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 puts 'This is the first executed statement'
 
 # I am a comment
 a = 1 + (2 * 3)
 
-if a == 6
-  puts 'Will I be printed?!'
-end
+puts 'Will I be printed?!' if a == 6
 
 puts 'The end!'
