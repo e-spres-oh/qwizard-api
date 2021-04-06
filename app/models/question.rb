e59609cba85770e101433a44ce77611e3af901de
Dan Percic <danpercic86@gmail.com>
2021-04-06 11:54:44 +0300

    add question model and migrations

---
+++ app/models/question.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Question < ApplicationRecord
+  belongs_to :quiz
+end
--- db/migrate/20210216210730_create_quizzes.rb
+++ db/migrate/20210216210730_create_quizzes.rb
@@ -2,7 +2,6 @@ class CreateQuizzes < ActiveRecord::Migration[6.1]
   def change
     create_table :quizzes do |t|
       t.string :title
-
       t.timestamps
     end
   end
