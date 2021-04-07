1bd49260e5afcf928c300d50167e2185390b6051
Adrian-Cristian BATRIN <cristianbatrin@gmail.com>
2021-04-07 15:59:16 +0300

    Quiz is now allowed blank values instead of just nil

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,7 +3,7 @@
 class Quiz < ApplicationRecord
   has_many :questions, inverse_of: :quiz
   has_one :lobby
-  validates :questions, presence: true, allow_nil: true
+  validates :questions, presence: true, allow_blank: true
   validates :lobby, presence: true, allow_nil: true
   validates :title, presence: true
 end
