55c40279678f1f3ec8990ca6dbf7d303b1f713a7
Ana <anaiv9cs@yahoo.co.uk>
2021-04-07 09:26:12 +0300

    Question tests added

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  it 'is valid if title, answer type, order, points and time limit present' do
+    quiz = Quiz.new(title: 'Test')
+    question = described_class.new(title: 'test', answer_type: 2, order: 2, points: 2, time_limit: 2, quiz: quiz)
+    expect(question).to be_valid
+  end
+
+  it 'is invalid if not binded to quiz' do
+    question = described_class.new(title: 'test', answer_type: 2, order: 2, points: 2, time_limit: 2)
+    expect(question).not_to be_valid
+  end
+
+  it 'is invalid if title, answer type, order, points and time limit missing' do
+    question = described_class.new
+    expect(question).not_to be_valid
+  end
+
+  it 'is invalid if title missing' do
+    question = described_class.new(answer_type: 2, order: 2, points: 2, time_limit: 2)
+    expect(question).not_to be_valid
+  end
+end
