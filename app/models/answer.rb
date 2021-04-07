2be455b0c026107cb357b3529fe8a0b1c209aa40
Tania Topciov <taniatopciov@yahoo.com>
2021-04-07 16:29:46 +0300

    Added  Answer Model and Created Suitable Migrations

---
+++ app/models/answer.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Answer < ApplicationRecord
+    validates :title, presence: true
+  
+    belongs_to :question
+end
---
+++ db/migrate/20210406181254_create_answers.rb
@@ -0,0 +1,9 @@
+class CreateAnswers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :answers do |t|
+      t.boolean :is_correct
+      t.string :title
+      t.timestamps
+    end
+  end
+end
