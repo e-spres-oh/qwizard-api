7f2ec2a1ad51b8d32e86d5c791353bab500363a3
Adrian-Cristian BATRIN <cristianbatrin@gmail.com>
2021-04-13 18:57:56 +0300

    Changed lobby status to enum

--- app/models/lobby.rb
+++ app/models/lobby.rb
@@ -2,9 +2,11 @@
 
 class Lobby < ApplicationRecord
   belongs_to :quiz
-  has_many :players
+  has_many :players, dependent: :destroy
   validates :quiz, presence: true
   validates :code, presence: true
   validates :current_question_index, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :status, presence: true, numericality: { only_integer: true }
+
+  enum status: [:pending, :in_progress, :finished]
 end
