182227ea7d24f1136c6169f9fa58eda31dbd18aa
Ana <anaiv9cs@yahoo.co.uk>
2021-04-13 21:47:24 +0300

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :quizzes
+      resources :questions
+      resources :lobbies
+      resources :players
+      resources :answers
+      resources :player_answers
     end
   end
 end
