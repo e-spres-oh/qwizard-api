806ebe3b4fa05582b1dd172d95cfc705ecfd62e2
Alex Prapugicu <40821161+AlexPrapugicu@users.noreply.github.com>
2021-04-14 13:30:01 +0300

    created questions controller

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class QuestionsController < ApplicationController
+      def index
+        @questions = Question.all
+        render :index
+      end
+
+      def create
+        @question = Question.new(question_params)
+
+        if @question.save
+          render :show, status: :created
+        else
+          render 'api/v1/model_errors', locals: { errors: @question.errors }, status: :unprocessable_entity
+        end
+      end
+
+      def show
+        @question = Question.find(params[:id])
+        render :show
+      end
+
+      def update
+        @question = Question.find(params[:id])
+
+        if @question.update(question_params)
+          render :show
+        else
+          render 'api/v1/model_errors', locals: { errors: @question.errors }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @question = Question.find(params[:id])
+        @question.destroy
+
+        render :show
+      end
+
+      private
+
+      def question_params
+        params.require(:question).permit(:title,:time_limit,:points,:answer_type,:order,:quiz_id)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :quizzes
+      resources :questions
     end
   end
 end
