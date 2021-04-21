26aa27aa77de510c0ca0303c44fb91ea9e7a9112
Alexandru Sava <asava.incoming@fh-salzburg.ac.at>
2021-04-21 11:59:26 +0200

    Add Lobbies controller, views and route

---
+++ app/controllers/api/v1/lobbies_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class LobbiesController < ApplicationController
+      def index
+        @lobbies = Lobby.all
+        render :index
+      end
+
+      def create
+        @lobby = Lobby.new(lobby_params)
+
+        if @lobby.save
+          render :show, status: :created
+        else
+          render 'api/v1/model_errors', locals: { errors: @lobby.errors }, status: :unprocessable_entity
+        end
+      end
+
+      def show
+        @lobby = Lobby.find(params[:id])
+        render :show
+      end
+
+      def update
+        @lobby = Lobby.find(params[:id])
+
+        if @lobby.update(question_params)
+          render :show
+        else
+          render 'api/v1/model_errors', locals: { errors: @lobby.errors }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @lobby = Lobby.find(params[:id])
+        @lobby.destroy
+
+        render :show
+      end
+
+      private
+
+      def lobby_params
+        params.require(:lobby).permit(:current_question_index, :status, :code, :quiz_id)
+      end
+    end
+  end
+end
---
+++ app/views/api/v1/lobbies/_lobby.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! lobby, :id, :current_question_index, :status, :code, :created_at, :updated_at
