bd83be5bb9d3e237bc0b55be0761700aea315e58
Alexandru Sava <asava.incoming@fh-salzburg.ac.at>
2021-04-21 15:53:24 +0200

    Add nested resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,16 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :quizzes do
-        resources :questions, shallow: true
+        resources :questions, shallow: true do
+          resources :answers, shallow: true
+        end
+        resources :lobbies, shallow: true do
+          resources :players, shallow: true do
+            resources :player_answers, shallow: true
+          end
+        end
       end
 
-      resources :answers
-      resources :lobbies
-      resources :players
-      resources :player_answers
       resources :users
 
       post :login, to: 'sessions#login'
