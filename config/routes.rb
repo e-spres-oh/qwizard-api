5fae79c999c94f78fffdf0ca17910c411eb8e810
Alex Prapugicu <40821161+AlexPrapugicu@users.noreply.github.com>
2021-04-21 12:33:13 +0300

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,25 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
+      resources :users
+      
       resources :quizzes do
-        resources :questions, shallow: true
+        
+        resources :questions, shallow: true do
+          resources :answers, shallow: true do
+            # TODO: define a way to handle this path too
+            # resources :player_answers, shallow: true 
+          end
+        end
+        
+        resources :lobbies, shallow: true do
+          resources :players, shallow: true do
+            resources :player_answers, shallow: true
+          end
+        end
+
       end
 
-      resources :answers
-      resources :lobbies
-      resources :players
-      resources :player_answers
-      resources :users
 
       post :login, to: 'sessions#login'
       get :me, to: 'sessions#me'
