a2ae8510ef687daab41ccab0fbe937de1de6cf31
Sorin Guga <sorin.guga@unifiedpost.com>
2021-05-06 11:56:51 +0300

    week-5-homework - Fixes Rubocop issues

--- app/controllers/api/v1/lobbies_controller.rb
+++ app/controllers/api/v1/lobbies_controller.rb
@@ -8,27 +8,16 @@ module Api
       before_action :set_quiz, only: [:index, :create]
 
       def answer
-        lobby = Lobby.find(params[:id])
         player = Player.find_by(id: params[:player_id])
 
         return head :not_found if player.blank?
 
-        question = Question.find_by(quiz_id: lobby.quiz_id, order: lobby.current_question_index)
-        answer_ids = Answer.where(question_id: question.id).map(&:id)
-        PlayerAnswer.where(player: player, answer_id: answer_ids).destroy_all
-
-        params[:answers].each do |id|
-          answer = Answer.find(id)
-          PlayerAnswer.create(player: player, answer: answer)
-        end
-
         lobby = Lobby.find(params[:id])
-        players = lobby.players.to_a.select do |p|
-          p.player_answers.any? { |pa| pa.answer.question.order == lobby.current_question_index }
-        end
-        Pusher.trigger(lobby.code, Lobby::ANSWER_SENT, { answer_count: players.count })
-
         question = lobby.quiz.questions.find_by(order: lobby.current_question_index)
+
+        create_player_answers!(player, question)
+        notify_answer_count(lobby)
+
         @answers = question.answers
         render :answer
       end
@@ -108,6 +97,22 @@ module Api
 
       def set_quiz
         @quiz = Quiz.find(params[:quiz_id])
+      end
+
+      def create_player_answers!(player, question)
+        PlayerAnswer.where(player: player, answer_id: question.answers.map(&:id)).destroy_all
+
+        params[:answers].each do |id|
+          answer = Answer.find(id)
+          PlayerAnswer.create(player: player, answer: answer)
+        end
+      end
+
+      def notify_answer_count(lobby)
+        players = lobby.players.to_a.select do |p|
+          p.player_answers.any? { |pa| pa.answer.question.order == lobby.current_question_index }
+        end
+        Pusher.trigger(lobby.code, Lobby::ANSWER_SENT, { answer_count: players.count })
       end
 
       def player_params
