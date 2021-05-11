5e5d2c77c48bf3f668afa834966bc940cf9f0913
Tania Topciov <taniatopciov@yahoo.com>
2021-05-11 23:17:35 +0300

    Added Endpoints

--- app/controllers/api/v1/lobbies_controller.rb
+++ app/controllers/api/v1/lobbies_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class LobbiesController < AuthenticatedController
       before_action :require_authentication, except: [:from_code, :join]
-      before_action :require_authorisation, only: [:show, :update, :destroy]
+      before_action :require_authorisation, only: [:show, :update, :destroy, :start]
       before_action :set_quiz, only: [:index, :create]
 
       def join
@@ -22,6 +22,61 @@ module Api
         return head :not_found if @lobby.nil?
 
         render :show
+      end
+
+      def start
+        @lobby = Lobby.find(params[:id])
+
+        if @lobby.update(status: :in_progress)
+          Pusher.trigger(@lobby.code, Lobby::LOBBY_START, {})
+          render :show
+        else
+          render 'api/v1/model_errors', locals: { errors: @lobby.errors }, status: :unprocessable_entity
+        end
+      end
+
+      def answer
+        lobby = Lobby.find(params[:id])
+
+        player = Player.find_by(id: params[:player_id])
+
+        return head :not_found if player.nil?
+
+        current_question = lobby.quiz.questions.find_by(order: lobby.current_question_index)
+        @answers = current_question.answers
+
+        current_question.answers.each do |answer|
+          PlayerAnswer.where(player: player, answer_id: answer.id).delete_all
+        end
+
+        create_player_answer_model(player)
+
+        send_answer_count_to_pusher(lobby)
+
+        render :answer
+      end
+
+      def create_player_answer_model(player)
+
+        answers_ids = params[:answers]
+        answers_ids.each do |answer_id|
+          answer = Answer.find(answer_id)
+          PlayerAnswer.create(player: player, answer: answer)
+        end
+
+      end
+
+      def send_answer_count_to_pusher(lobby)
+
+        players_list = lobby.players.to_a
+        players_list.each do |player|
+          player.player_answers.any? do |player_answer|
+            player_answer.answer.in?(@answers)
+          end
+        end
+
+        Pusher.trigger(lobby.code, Lobby::ANSWER_SENT, { answer_count: players_list.count })
+
       end
 
       def index
